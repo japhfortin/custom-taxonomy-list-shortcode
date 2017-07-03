044dd1919ec8e01ba30cc9ad080c0b5681552590
japhfortin <japhfortin@gmail.com>
2017-07-03 16:42:09 +0800

    Remove script comments

--- custom-taxonomy-list-shortcode.php
+++ custom-taxonomy-list-shortcode.php
@@ -118,11 +118,6 @@ add_shortcode( 'custom_taxonomy_list', 'jf_custom_taxonomy_list' );
 /**
  * Enqueue scripts
  *
- * @param string $handle Script name
- * @param string $src Script url
- * @param array $deps (optional) Array of script names on which this script depends
- * @param string|bool $ver (optional) Script version (used for cache busting), set to null to disable
- * @param bool $in_footer (optional) Whether to enqueue the script before </head> or before </body>
  */
 function jf_plugin_scripts_styles() {
     wp_enqueue_style( 'ctl-style', CTL_URL . 'css/style.css', array(), '1.0' );
